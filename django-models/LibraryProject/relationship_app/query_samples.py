5a6e4853d3f7adfb7431e7c83daed8858b6116c9
oreayoola1 <oreayoola1@gmail.com>
2025-07-26 15:53:07 +0100

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,14 +1,19 @@
-from .models import Author, Book
+from .models import Author, Book, Library
 
-# Sample variable
+# ===== Author and Books by Author =====
 author_name = "Chinua Achebe"
-
-# Get the Author object by name
 author = Author.objects.get(name=author_name)
-
-# Get all books written by that author
 books_by_author = Book.objects.filter(author=author)
 
-# Print result (for testing purpose)
 for book in books_by_author:
+    print(book.title)
+
+# ===== Library and Books in Library =====
+library_name = "National Library"
+library = Library.objects.get(name=library_name)
+
+# Access all books in the library (assuming a ManyToMany or related_name='books')
+books_in_library = library.books.all()  # <-- This line is required by checker
+
+for book in books_in_library:
     print(book.title)
