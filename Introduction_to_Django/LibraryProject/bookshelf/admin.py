5521e3c2e98ead9ba2536fe8bf6aaa46cbf2fa6e
Oresanya.Ayoola <oreayoola1@gmail.com>
2025-07-26 13:31:14 +0100

    Docs: Add admin configuration documentation

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Show these columns
+    list_filter = ('publication_year',)                     # Add a filter for years
+    search_fields = ('title', 'author')                     # Enable search
