be904d293f63619b66aea83cb645c693797926a4
oreayoola1 <oreayoola1@gmail.com>
2025-07-26 16:33:22 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,3 +4,10 @@ from .models import Book
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
+from django.views.generic import DetailView
+from .models import Library  # ✅ import is required
+
+class LibraryDetailView(DetailView):
+    model = Library  # ✅ model reference
+    template_name = "relationship_app/library_detail.html"  # ✅ template path
+    context_object_name = "library"  # ✅ variable expected in template
