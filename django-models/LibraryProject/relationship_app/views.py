b714b82c732fdcd2db2efcd8be396e7e28580298
oreayoola1 <oreayoola1@gmail.com>
2025-07-27 17:26:48 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,6 @@
 # relationship_app/views.py
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library
