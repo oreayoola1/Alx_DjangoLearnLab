3f688cb16d9d3a7bef3617af55ae930aa7eb7026
oreayoola1 <oreayoola1@gmail.com>
2025-07-27 17:17:10 +0100

    Update views.py

--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -1,14 +1,14 @@
+from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
 
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
-            return redirect('list_books')  # redirect to wherever makes sense
+            login(request, user)  # Automatically log in after registration
+            return redirect('list_books')  # or wherever you'd like to redirect
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
