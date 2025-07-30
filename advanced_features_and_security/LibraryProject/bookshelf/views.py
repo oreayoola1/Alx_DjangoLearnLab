78c74601a57cbdafdaef7da1491de1f06e2d9c75
Oresanya.Ayoola <oreayoola1@gmail.com>
2025-07-30 22:44:43 +0100

    Import ExampleForm into views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
