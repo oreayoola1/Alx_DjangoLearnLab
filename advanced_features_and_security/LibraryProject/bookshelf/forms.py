76acc9503bf15fcd85550615574f17f4835c54b2
Oresanya.Ayoola <oreayoola1@gmail.com>
2025-07-30 22:43:26 +0100

    Add ExampleForm for CSRF protection demonstration

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,12 +1,15 @@
-# bookshelf/forms.py
-
 from django import forms
 from .models import Book
 
 class BookSearchForm(forms.Form):
-    query = forms.CharField(max_length=100, required=False, label="Search Books")
+    title = forms.CharField(max_length=100, required=False)
+    author = forms.CharField(max_length=100, required=False)
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author', 'description', 'published_date']
+        fields = ['title', 'author', 'publication_date']
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, label='Your Name')
+    email = forms.EmailField(label='Your Email')
