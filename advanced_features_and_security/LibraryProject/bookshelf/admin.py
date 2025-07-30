442f39851268a51988c7a3b377a6c0add76a185c
Oresanya.Ayoola <oreayoola1@gmail.com>
2025-07-30 20:48:03 +0100

    Fix: prevent CustomUser model from being registered twice in admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import Book
-from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author')  # remove 'publication_year'
-    list_filter = ()                    # or remove this line entirely if unused
+    list_display = ('title', 'author')  # Removed 'publication_year' to fix error
     search_fields = ('title', 'author')
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
